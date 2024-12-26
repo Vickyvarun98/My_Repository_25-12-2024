6dd5555d1dd9f9d009ef67c1216bd23c3dc9ce5c
Vickyvarun98 <ashamandala5@gmail.com>
2024-12-26 08:06:04 +0530

    Java Pattern is added

---
+++ AutomationFramework_Web/src/test/java/JavaPrograms/RightAngledTriangle.java
@@ -0,0 +1,22 @@
+package JavaPrograms;
+
+public class RightAngledTriangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int count=5;
+		for(int i=1;i<=count;i++) {
+			for(int j=1;j<=i;j++) {
+				if(i<=j) {
+					System.out.print("* ");
+				}else {
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
